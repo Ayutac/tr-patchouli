c368bb87f4ee818ef496b76f41f421862c4eeeea
Ayutac <skoch02@students.uni-mainz.de>
2022-05-10 22:15:02 +0200

    custom book doesn't work as intended

--- src/main/java/org/abos/fabricmc/trpatchouli/TRPatchouli.java
+++ src/main/java/org/abos/fabricmc/trpatchouli/TRPatchouli.java
@@ -1,8 +1,12 @@
 package org.abos.fabricmc.trpatchouli;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import techreborn.TechReborn;
 
 public class TRPatchouli implements ModInitializer {
 
@@ -10,8 +14,11 @@ public class TRPatchouli implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final GuideBook guide = new GuideBook(new FabricItemSettings().group(TechReborn.ITEMGROUP));
+
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID,"guide"), guide);
 		LOGGER.info("%s has been initialized!".formatted(MOD_ID));
 	}
 }
